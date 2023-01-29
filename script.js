4550c54d73d6f50f4dd7dd8258e23b06cb965cfe
Wei Neng Vernon Koh <s10248408@connect.np.edu.sg>
2023-01-30 02:34:37 +0800

    Added about us page and Login forms

--- script.js
+++ script.js
@@ -79,3 +79,11 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+fadeOut();
+function loader(){
+    document.querySelector('.loader-container').classList.add('active');
+}
+
+function fadeOut(){
+    setTimeout(loader,1000);
+}
